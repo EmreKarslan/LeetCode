ec2388d24ef60f1adfd1722eda508651941634e1
Emre KÜÇÜKARSLAN <kucukarslanemre@gmail.com>
2024-04-04 23:58:44 +0300

    Solution: Maximum Nesting Depth of the Parentheses(TypeScript)

---
+++ Problems/1614. Maximum Nesting Depth of the Parentheses/maximum-nesting-depth-of-the-parentheses.ts
@@ -0,0 +1,16 @@
+//Solution 1: 51ms 50.78 MB
+function maxDepth(s: string): number {
+  let maxDepth = 0;
+  let currentDepth = 0;
+
+  for (const char of s) {
+    if (char === "(") {
+      currentDepth++;
+      maxDepth = Math.max(maxDepth, currentDepth);
+    } else if (char === ")") {
+      currentDepth--;
+    }
+  }
+
+  return maxDepth;
+}
